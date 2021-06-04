bfd41cf8eefa73c6e8c90b549a7a0d538f11ae09
raghunandanas <40174367+raghunandanas@users.noreply.github.com>
2021-06-04 14:12:21 +0530

    Create runUAT_ragz.bash

---
+++ dir6/runUAT_ragz.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set variables
+hostname='localhost'
+port=$1
+
+# wait for the app to start
+sleep 5 
+
+# ping the app
+status_code=$port
+
+if [ $status_code == 88 ];
+then
+	echo "PASS: ${hostname}:${port} is reachable"
+else
+	echo "FAIL: ${hostname}:${port} is unreachable"
+    exit 1
+fi
